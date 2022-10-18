1e6041f16ef04c4277272ae385737a9189b182dc
Martin Vehmas <martin.vehmas@viaecole.se>
2022-10-18 17:55:23 +0200

    First version. Only printing app setting.

---
+++ LpApiIntegration/LpApiIntegration.FetchFromV2/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using LpApiIntegration.FetchFromV2;
+
+using IHost host = Host.CreateDefaultBuilder(args).Build();
+IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+
+ApiSettings apiSettings = config.GetRequiredSection("ApiSettings").Get<ApiSettings>();
+
+Console.WriteLine(apiSettings.ClientSecret);
+
+
+// Application code should start here.
+
+await host.RunAsync();
