a0ea6c4135293b935ff0c1746c5fa3f7ad90d125
Andreas <andreas.naslund@edu.edugrade.se>
2022-11-01 13:57:03 +0100

    Added CourseModel Added DbSet to DbContext

---
+++ LpApiIntegration/LpApiIntegration.FetchFromV2/Db/Models/CourseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LpApiIntegration.FetchFromV2.Db.Models
+{
+    internal class CourseModel
+    {
+        [Key] public int DbId { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; } 
+        public DateTime? LifespanFrom { get; set; }
+        public DateTime? LifespanUntil { get; set; }
+        //public int? Points { get; set; }
+    }
+}
